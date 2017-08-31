9d79d99a20765966f73e5b1ba18730118d41c7eb
John Cato <sora4kairi24@gmail.com>
2017-08-31 13:19:49 +0200

    Update Kalkulator.cpp

--- Kalkulator/Kalkulator.cpp
+++ Kalkulator/Kalkulator.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-	double x, y;
+	double y;
 	std::string line = "";
 	std::string line2 = "";
 	double sum;
@@ -38,16 +38,26 @@ int main()
 		}
 		else
 		{
-			std::cout << "Skriv inn en akseptert operator neste gang!";
+			std::cout << "Skriv inn en akseptert operator neste gang!" << std::endl;
 		};
-		std::cout << "Summen er: " << sum << std::endl;
+		std::cout << "Den totale summen er: " << sum << std::endl;
 		std::cout << "Vil du fortsette? Skriv \"y\" hvis du vil fortsette eller \"n\" hvis du er ferdig:" << std::endl;
+		std::cout << "Hvis du vil slette summen din, skriv \"s\". Dette vil slette summen din og starte programmet på nytt." << std::endl;
 		std::cin >> line2;
-		if (line2 == "n") 
+		if (line2 == "n")
 		{
 			end = true;
 		}
+		else if (line2 == "s")
+		{
+			std::cout << "Skriv inn et nummer: ";
+			std::cin >> sum;
+		}
+		else
+		{
+			std::cout << "Programmet vil nå fortsette" << std::endl;
+		}
+
 	}
     return 0;
 }
-
